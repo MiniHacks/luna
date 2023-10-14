3d3af07a7799861170cfdde0d6455d005f11cff8
cbbor <c.b.borisova@gmail.com>
2023-10-14 09:07:12 -0400

    ❤modified functions and examples

--- packages/electron/lib/functions/askUser.ts
+++ packages/electron/lib/functions/askUser.ts
@@ -1,22 +1,26 @@
 // import { createChild } from "../../../main.js";
 import { Page } from "puppeteer-core";
+import askUserJSON from "@luna/model/functions/askUser.json";
+import { type } from "os";
 
-export const askUser = {
-  name: "askUser",
-  description:
-    "Prompt the user for input. The user's response can be used to fill out a form, answer a question, or click the right button.",
-  parameters: {
-    type: "object",
-    properties: {
-      question: {
-        type: "string",
-        description:
-          "The question to display to the user. Questions should be phrased as if they were being asked by a human.",
-      },
-    },
-    required: ["message"],
-  },
-};
+export const askUser = askUserJSON;
+
+// export const askUser = {
+//   name: "askUser",
+//   description:
+//     "Prompt the user for input. The user's response can be used to fill out a form, answer a question, or click the right button.",
+//   parameters: {
+//     type: "object",
+//     properties: {
+//       question: {
+//         type: "string",
+//         description:
+//           "The question to display to the user. Questions should be phrased as if they were being asked by a human.",
+//       },
+//     },
+//     required: ["message"],
+//   },
+// };
 
 type AskUserArgs = {
   question: string;
