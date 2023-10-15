b27721e92b42ccf3be4fca4a3cf7a108b21fb50a
cbbor <c.b.borisova@gmail.com>
2023-10-14 22:58:29 -0400

    � fixing jsons

--- packages/electron/lib/config.ts
+++ packages/electron/lib/config.ts
@@ -3,5 +3,5 @@ export const CHROME_DEBUGGING_PORT = 9223;
 export enum MODELS {
   GPT3 = "gpt-3.5-turbo-0613",
   GPT4 = "gpt-4-0613",
-  FINE_TUNED = "gpt-4",
+  FINE_TUNED = "ft:gpt-3.5-turbo-0613:samyok::89lRGl0i",
 }
--- packages/model/scripts/create_training_data.py
+++ packages/model/scripts/create_training_data.py
@@ -1,6 +1,7 @@
 import os
 import copy
 import json
+import re
 
 # iterate over files in
 # that directory
@@ -9,7 +10,7 @@ for filename in os.listdir("../functions"):
     f = os.path.join("../functions", filename)
     # checking if it is a file
     if os.path.isfile(f):
-        file = open(f, "r")
+        file = open(f, "r", encoding="utf-8")
         functions.append(json.load(file))
         file.close()
 
@@ -19,13 +20,32 @@ with open("../data/finetuning_data.jsonl", mode='w', encoding='utf-8') as finetu
         f = os.path.join("../data/trainingdata", filename)
         # checking if it is a file
         if os.path.isfile(f):
-            file = open(f, "r")
+            print(filename)
+            file = open(f, "r", encoding="utf-8")
             data = copy.deepcopy(json.loads(file.read()))
-            goal = data["messages"][0]["content"]
+            print("original: ", data["messages"][0]["content"])
+            goal = re.sub("(.*(L|l)una[^A-z0-9\s]*)*(.*please[^A-z0-9\s]*)*(.*you)*(.*please[^A-z0-9\s]*)*", "", data["messages"][0]["content"])
+            print("goal: ", goal)
+            print("________________________________\n\n")
             data["messages"][0]["content"] = system + " " + goal
             data["functions"] = functions
             finetuningf.write(json.dumps(data) + "\n")
             file.close()
+        else:
+            for filename_2 in os.listdir(f):
+                f_2 = os.path.join(f, filename_2)
+                if os.path.isfile(f_2):
+                    print(filename_2)
+                    file_2 = open(f_2, "r", encoding="utf-8")
+                    data_2 = copy.deepcopy(json.loads(file_2.read()))
+                    print(data_2["messages"][0]["content"])
+                    goal_2 = re.sub("(.*(L|l)una[^A-z0-9\s]*)*(.*please[^A-z0-9\s]*)*(.*you)*(.*please[^A-z0-9\s]*)*", "", data_2["messages"][0]["content"])
+                    print(goal_2)
+                    print("________________________________\n\n")
+                    data_2["messages"][0]["content"] = system + " " + goal_2
+                    data_2["functions"] = functions
+                    finetuningf.write(json.dumps(data_2) + "\n")
+                    file_2.close()
 finetuningf.close()
 
 
